75bbed7ccc02219229489f3af905fdc310ac95a6
huayong94 <37759030+huayong94@users.noreply.github.com>
2023-09-25 10:55:24 +0800

    Delete DataLoader directory

--- DataLoader/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Caseset import Caseset
-from .Dataset import Collate, CollateGPU, Dataset
-from .TransformForGPU import (CentralCropTensor, NormalizeTensor,
-                              RandomAffineTransform,RandomMirrorTensor2D)
-
